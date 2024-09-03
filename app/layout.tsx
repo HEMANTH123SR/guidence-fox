712542b09e07240e021ebcd96027b9de341b1628
Kundan Kumar Sahu <espkundan@gmail.com>
2024-09-03 14:28:25 +0530

    added auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
-import type { Metadata } from "next";
-import { dio_font_normale } from "@/lib/fonts/font";
-import { NavBar } from "@/app/components/NavBar";
-import "./globals.css";
-import Zlayout from "./components/Zlayout";
-
-
+import type { Metadata } from 'next';
+import { dio_font_normale } from '@/lib/fonts/font';
+import { NavBar } from '@/app/components/NavBar';
+import './globals.css';
+import Zlayout from './components/Zlayout';
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
 
 export const metadata: Metadata = {
-  title: "guiding fox",
-  description: "",
+  title: 'guiding fox',
+  description: '',
 };
 
 export default function RootLayout({
@@ -17,12 +22,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${dio_font_normale.className} w-screen bg-[#FEFEFF] overflow-x-hidden`}>
-        <NavBar />
-        {children}
-        <Zlayout />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${dio_font_normale.className} w-screen bg-[#FEFEFF] overflow-x-hidden`}
+        >
+          <NavBar />
+          {children}
+          <Zlayout />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
